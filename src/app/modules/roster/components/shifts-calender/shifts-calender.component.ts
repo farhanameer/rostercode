ac37eefa4c3a244af5ad7d168b7b895d1d0b5d0d
ibtesam.tahir.appsgenii <ibtesam.tahir@appsgenii.eu>
2022-09-06 19:49:09 +0500

    After usman changes

--- src/app/modules/roster/components/shifts-calender/shifts-calender.component.ts
+++ src/app/modules/roster/components/shifts-calender/shifts-calender.component.ts
@@ -1,3 +1,4 @@
+import { CalendarService } from './../../../../services/calander.service';
 import  moment  from 'moment';
 import { Component, OnInit } from '@angular/core';
 
@@ -11,20 +12,48 @@ export class ShiftsCalenderComponent implements OnInit {
 
   months = new Array(5);
   currentDate: any;
+  currentMonthDates: any;
   weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-  constructor() { }
+  constructor(private calendar: CalendarService) { }
 
   ngOnInit(): void {
       this.currentDate = moment();
-
+      this.currentMonthDates = this.calendar.getCalendar(
+        moment(this.currentDate).format('YYYY'), 
+        moment(this.currentDate).format('MMM'), 
+        this.weekDays
+        );
       this.months[0] = moment(this.currentDate).subtract(2,'month').format('MMMM YYYY');
       this.months[1] = moment(this.currentDate).subtract(1,'month').format('MMMM YYYY');
       this.months[2] = moment(this.currentDate).format('MMMM YYYY');
       this.months[3] = moment(this.currentDate).add(1,'month').format('MMMM YYYY');
       this.months[4] = moment(this.currentDate).add(2,'month').format('MMMM YYYY');
 
+      this.currentMonthDates = this.arrayOfArrays(this.currentMonthDates);
       console.log(this.months);
+      //debugger;
+      console.log(this.currentMonthDates);
+  }
+
+
+  calendarArray : any = [];
+
+  arrayOfArrays(array){
+    let resultingArray = [];
+    const responseArray = [];
+    const numberOfIterations = array.length / 7;
+    let counter = 0;
+    for(let i=1; i<= numberOfIterations; i++){
+      resultingArray = [];
+      for(let j=1; j<=7; j++){
+        resultingArray.push(array[counter]);
+        counter = counter + 1;
+      }
+      responseArray.push([...resultingArray]);
+    }
+
+    return responseArray;
   }
 
   getMonthAndYear(isForwarding = false){
@@ -51,7 +80,8 @@ export class ShiftsCalenderComponent implements OnInit {
       console.log(this.months); 
     }
 
-    //this.calender.getCalendar(moment(this.currentDate).format('YYYY'), moment(this.currentDate).format('MMM'), this.weekDays);
+    this.currentMonthDates = this.calendar.getCalendar(moment(this.currentDate).format('YYYY'), moment(this.currentDate).format('MMM'), this.weekDays);
+    this.currentMonthDates = this.arrayOfArrays(this.currentMonthDates);
   }
 
 }
