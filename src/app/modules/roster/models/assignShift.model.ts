eba398ba77ab8536e716c893184a75b8a597b23f
HZuberi824 <hashimzuberi6@gmail.com>
2022-09-22 19:37:15 +0500

    update

---
+++ src/app/modules/roster/models/assignShift.model.ts
@@ -0,0 +1,8 @@
+export class AssignShiftModel{
+    client_id:number;
+    shift_id:number;
+    employee_id:number;
+    rosterDate:Date;
+    additional_shift_id:number;
+    linemanager_id:number;
+}
