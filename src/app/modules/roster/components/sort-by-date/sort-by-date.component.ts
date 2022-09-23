05c1b70a610b1b321fc19dc07077ecbab453ddc8
Zeeshan Malik <malikzee979@gmail.com>
2022-09-23 12:50:15 +0500

    bug fixedc

--- src/app/modules/roster/components/sort-by-date/sort-by-date.component.ts
+++ src/app/modules/roster/components/sort-by-date/sort-by-date.component.ts
@@ -1,64 +1,62 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AppLocalStorageService } from 'src/app/services/app-local-storage.service';
 import { forEachChild } from 'typescript';
 import { RosterService } from '../../services/data/rosterView.data.service';
 
 @Component({
   selector: 'app-sort-by-date',
   templateUrl: './sort-by-date.component.html',
-  styleUrls: ['./sort-by-date.component.css']
+  styleUrls: ['./sort-by-date.component.css'],
 })
 export class SortByDateComponent implements OnInit {
+  @Input() dates: any;
 
+  data: any = [];
 
-  @Input() dates:any;
-
-  data : any = [
-   
-  ]
-
-  constructor(public activeModal: NgbActiveModal , private dataService: RosterService) { }
+  constructor(
+    public activeModal: NgbActiveModal,
+    private dataService: RosterService,
+    private appLocalStorage: AppLocalStorageService
+  ) {}
 
   ngOnInit(): void {
-
-    console.log('in popup' , this.dates);
-    this.getLMRosterView();
+    console.log('in popup', this.dates);
+    this.getLMRosterView({
+      client_id: this.appLocalStorage.getClientId(),
+      year_month: '2022-07',
+      is_roster_employees: 1,
+      reporting_to_id: this.appLocalStorage.getUserId(),
+    });
   }
 
+  async getLMRosterView(params) {
+    const data = await this.dataService.getLMRosterView(params);
 
-
-  async getLMRosterView(){
-    const data = await this.dataService.getLMRosterView({
-      'start_date' : this.dates.start , 
-      'end_date' : this.dates.end
-    });
-    
-    if(!data["data"]["status"]){
+    if (!data['data']['status']) {
       console.log('error a gya');
       return;
     }
 
-    
-    const response = data["data"]["payload"];
+    const response = data['data']['payload'];
     const hash = {};
     const transformedData = [];
 
-    response.forEach(singleData =>{
-        let employeeArray = [];
-        const obj = {
-          date : singleData.date , 
-          employees : []
-        };
+    response.forEach((singleData) => {
+      let employeeArray = [];
+      const obj = {
+        date: singleData.date,
+        employees: [],
+      };
 
-        singleData.shifts.forEach(singleShift =>{
-          employeeArray = [...employeeArray , ...singleShift.employees];
-        });
+      singleData.shifts.forEach((singleShift) => {
+        employeeArray = [...employeeArray, ...singleShift.employees];
+      });
 
-        obj.employees = employeeArray;
-        this.data.push(obj);
+      obj.employees = employeeArray;
+      this.data.push(obj);
     });
 
-    console.log('loopAble Data' , this.data);
+    console.log('loopAble Data', this.data);
   }
-
 }
