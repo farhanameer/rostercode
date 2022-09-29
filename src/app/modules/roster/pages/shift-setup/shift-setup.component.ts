527e8ddc098f42288446f16d04acdccab50b83d2
ibtesam.tahir.appsgenii <ibtesam.tahir@appsgenii.eu>
2022-09-29 15:08:41 +0500

    29 sep

--- src/app/modules/roster/pages/shift-setup/shift-setup.component.ts
+++ src/app/modules/roster/pages/shift-setup/shift-setup.component.ts
@@ -1,3 +1,4 @@
+import { ShiftRequestDataService } from './../../services/data/shiftRequest.data';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShiftSetupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shiftRequestService: ShiftRequestDataService) { }
+
+  shiftTypeArray : any;
+  shiftColorArray: any;
 
   ngOnInit(): void {
+    // debugger;
+    this.getShiftTypes();
+    this.getShiftColors();
+    // this.getShift
+  }
+
+  async getShiftTypes(){
+    const data = await this.shiftRequestService.getShiftTypes();
+    let shifts = data["data"]["payload"];
+    if(!Array.isArray(shifts)){
+      console.log('error occured');
+    }
+    const shiftsArray = [];
+    shifts.forEach(shift =>{
+      shiftsArray.push({
+        id : shift.shift_type_id , 
+        name : shift.shift_type_name
+      })
+    });
+    console.log(shiftsArray);
+    this.shiftTypeArray = shiftsArray;
+    console.log(this.shiftTypeArray);
+
+  }
+
+  async getShiftColors(){
+    const data = await this.shiftRequestService.getShiftColors();
+    let colors = data["data"]["payload"];
+    if(!Array.isArray(colors)){
+      console.log('error occured');
+    } 
+    const colorsArray = [];
+    let i = 0;
+    colors.forEach(color =>{
+      let obj = {
+        id: i,
+        name: color
+      }
+      colorsArray.push(obj);
+      i++;
+    });
+    console.log(colorsArray);
+    this.shiftColorArray = colorsArray;
+    console.log(this.shiftColorArray);
+
   }
 
 }
