2ee92025c1ce940ae164ec25f0c6418cea7f8a97
HZuberi824 <hashimzuberi6@gmail.com>
2022-09-29 14:58:37 +0500

    updated

--- src/app/modules/roster/pages/shift-setup/shift-setup.component.ts
+++ src/app/modules/roster/pages/shift-setup/shift-setup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-shift-setup',
@@ -12,28 +12,59 @@ export class ShiftSetupComponent implements OnInit {
 
   ngOnInit(): void {
     this.shiftSetUpForm=this.fb.group({
-      screen_role:[""],
-      client_id:[""],
-      color:[""],
-      name:[""],
-      time_in:[""],
-      time_out:[""],
-      mid_break_enable:[""],
-      mid_break_time_in:[""],
-      mid_break_time_out:[""],
-      ext_mid_break_day_id:[""],
-      ext_mid_break_time_in:[""],
-      ext_mid_break_time_out:[""],
-      consecutive_late:[""],
-      late_arrival_tolerance:[""],
-      attendance_tolerance:[""],
-      revert_shift_id:[""],
-      shift_revert_date_start:[""],
-      shift_revert_date_end:[""],
-      shift_type_id:[""]
+      screen_role:["",Validators.required],
+      client_id:["",Validators.required],
+      color:["",Validators.required],
+      name:["",Validators.required],
+      time_in:["",Validators.required],
+      time_out:["",Validators.required],
+      mid_break_enable:["",Validators.required],
+      mid_break_time_in:["",Validators.required],
+      mid_break_time_out:["",Validators.required],
+      ext_mid_break_day_id:["",Validators.required],
+      ext_mid_break_time_in:["",Validators.required],
+      ext_mid_break_time_out:["",Validators.required],
+      consecutive_late:["",Validators.required],
+      late_arrival_tolerance:["",Validators.required],
+      attendance_tolerance:["",Validators.required],
+      revert_shift_id:["",Validators.required],
+      shift_revert_date_start:["",Validators.required],
+      shift_revert_date_end:["",Validators.required],
+      shift_type_id:["",Validators.required],
+      glob_mkt_id:["",Validators.required],
+      region_id:["",Validators.required],
+      sub_region_id:["",Validators.required],
+      country_id:["",Validators.required],
+      state_id:["",Validators.required],
+      city_id:["",Validators.required],
+      branch_id:["",Validators.required],
+      department_id:["",Validators.required],
+      desg_id:["",Validators.required],
+      emp_id:["",Validators.required],
+      qrt_break:this.fb.array([])
 
 
     })
+  }
+
+  get qrt_break() : FormArray {
+    return this.shiftSetUpForm.get("qrt_break") as FormArray
+  }
+
+  newqrtbreak(): FormGroup {
+    return this.fb.group({
+      qrt_break_title: ["",Validators.required],
+      qrt_break_time_in: ["",Validators.required],
+      qrt_break_time_out:["",Validators.required]
+    })
+  }
+
+  get validateAForm(): any {
+    return this.shiftSetUpForm.controls
+  }
+
+  submit(){
+    console.warn(this.shiftSetUpForm.value)
   }
 
 }
