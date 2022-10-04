11ff7734ed89db2db3b518691dc859525a21a8a0
ibtesam.tahir.appsgenii <ibtesam.tahir@appsgenii.eu>
2022-10-04 18:55:39 +0500

    push

--- src/app/modules/roster/dialogs/single-shift-allocation/single-shift-allocation.dialog.ts
+++ src/app/modules/roster/dialogs/single-shift-allocation/single-shift-allocation.dialog.ts
@@ -1,7 +1,10 @@
+import { RosterService } from './../../services/data/rosterView.data.service';
+import { AppLocalStorageService } from './../../../../services/app-local-storage.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { EmployeeShiftDataService } from '../../services/data/dropdown.data';
+import moment from 'moment';
 
 @Component({
   selector: 'app-single-shift-allocation',
@@ -11,6 +14,7 @@ import { EmployeeShiftDataService } from '../../services/data/dropdown.data';
 export class SingleShiftAllocationDialog implements OnInit {
 
   singleShiftForm: FormGroup;
+  submitted : boolean = false;
 
   @Input() data;
 
@@ -18,7 +22,10 @@ export class SingleShiftAllocationDialog implements OnInit {
   
   constructor(
     public activeModal: NgbActiveModal,
-    private employeeShiftDataService: EmployeeShiftDataService,private fb: FormBuilder,
+    private employeeShiftDataService: EmployeeShiftDataService,
+    private fb: FormBuilder,
+    private appLocalStorage : AppLocalStorageService,
+    private rosterService : RosterService
   ) { }
 
   ngOnInit(): void {
@@ -41,7 +48,24 @@ export class SingleShiftAllocationDialog implements OnInit {
   }
 
   submit(){
-    console.warn(this.singleShiftForm.value)
+
+    this.singleShiftForm.value.date = moment(this.singleShiftForm.value.date).format("YYYY-MM-DD");
+
+    const body = {
+      "client_id" : this.appLocalStorage.getClientId(),
+      "linemanager_id" : this.appLocalStorage.getUserId(),
+      "shift_id" : 7,
+      "employee_id" : this.data.employee_id,
+      "rosterDate" : this.singleShiftForm.value.date,
+      "additional_shift_id" : this.singleShiftForm.value.allocateShift
+    }
+  debugger;
+    this.assignShift(body);
   }
+
+  async assignShift(body){
+    const res = await this.rosterService.assignAddtionalShift(body);
+    debugger;
+  }  
 
 }
