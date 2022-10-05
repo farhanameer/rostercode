03d66c9472534bfb7be4e39f6762863deb0d1321
Farhan Ameer <farhan.ameer@appsgenii.eu>
2022-10-05 14:59:55 +0500

    fixed

--- src/app/modules/roster/dialogs/overtime-adjustment/overtime-adjustment.dialog.ts
+++ src/app/modules/roster/dialogs/overtime-adjustment/overtime-adjustment.dialog.ts
@@ -4,11 +4,18 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AppLocalStorageService } from 'src/app/services/app-local-storage.service';
 import { RosterService } from '../../services/data/roster.dataService';
 
+
+
+
+
+
 @Component({
   selector: 'app-overtime-adjustment',
   templateUrl: './overtime-adjustment.dialog.html',
   styleUrls: ['./overtime-adjustment.dialog.css'],
 })
+
+
 export class OvertimeAdjustmentDialog implements OnInit {
   constructor(
     private fb: FormBuilder,
@@ -18,22 +25,62 @@ export class OvertimeAdjustmentDialog implements OnInit {
   ) {}
   @Input() modelData: any;
   overTimeForm: FormGroup;
-  type: string = 'cpl';
+  // type: string = 'cpl';
+
+   requiredIfValidator(predicate , customValue) {
+    return (formControl => {
+      console.log('running erveytime');
+      if (!formControl.parent) {
+        console.log('returned nulify Values');
+        return null;
+      }
+      if (predicate() == customValue) {
+        console.log('returned required value' , predicate());
+        return Validators.required(formControl); 
+      }
+      console.log('finally returned nulify values')
+      return null;
+    })
+  }
+
   ngOnInit(): void {
     console.log('employee Data', this.modelData);
     this.overTimeForm = this.fb.group({
       overTime: [this.modelData.overtime, Validators.required],
-      givenHours: ['', Validators.required],
+      givenHours: ['' , [this.requiredIfValidator(() => this.overTimeForm.get('type').value, 'payment')]],
       tillDate: ['', Validators.required],
-      addCPL: ['', Validators.required],
-      paymentForOvertime: ['', Validators.required],
-      cpl: ['', Validators.required],
-      payForHours: ['', Validators.required],
+      type : ['cpl'],
+      cpl: ['' , [this.requiredIfValidator(() => this.overTimeForm.get('type').value, 'cpl')]],
+    });
+
+    this.overTimeForm.get('type').valueChanges.subscribe((value) => {
+      this.overTimeForm.get('cpl').updateValueAndValidity();
+      this.overTimeForm.get('givenHours').updateValueAndValidity();
     });
     console.log(this.overTimeForm.value);
   }
+
+
+  
+  resetFormField(control){
+    
+      this.overTimeForm.get(control).markAsUntouched();
+      this.overTimeForm.get(control).setErrors(null);
+    
+  }
+  valueType : string  = 'cpl';
   overtimeType(val: string) {
-    this.type = val;
+    this.valueType = val;
+
+    // this.overTimeForm.get('type').setValue(val);
+    setTimeout(() => {
+      console.log(this.overTimeForm.value);
+    }, 5000);
+    // if(val == 'payment'){
+    //   this.resetFormField('cpl')
+    // }else if(val == 'cpl'){
+    //   this.resetFormField('givenHours')
+    // }
   }
 
   submit() {
@@ -47,17 +94,24 @@ export class OvertimeAdjustmentDialog implements OnInit {
     //   line_manager_id: this.appLocalStorage.getUserId(),
     // };
     // console.log('my obj', obj);
-    this.HoursAdjustMentTest();
+    this.HoursAdjustMent();
   }
-  async HoursAdjustMentTest() {
+  async HoursAdjustMent() {
     let obj = {
-      type: 'cpl',
+      type: this.valueType,
       employee_id: this.modelData.employee_id,
       givenHours: this.overTimeForm.value.givenHours,
       givenCPL: this.overTimeForm.value.cpl,
       client_id: this.appLocalStorage.getClientId(),
       line_manager_id: this.appLocalStorage.getUserId(),
     };
+
+    if(this.valueType == 'cpl'){
+      delete obj.givenHours;
+    }
+    if(this.valueType == 'payment'){
+      delete obj.givenCPL;
+    }
     // console.log('my obj', obj);
     const result = await this.overTimeAdjustment.getOvertimeAdjustment(obj);
     console.log('Overtime', result);
