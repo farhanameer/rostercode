3ea581a10b40d98fa555ba543648b79bbcacb8ca
ibtesam.tahir.appsgenii <ibtesam.tahir@appsgenii.eu>
2022-10-06 15:02:58 +0500

    pull

--- src/app/modules/roster/pages/shift-request-by-lm/shift-request-by-lm.component.ts
+++ src/app/modules/roster/pages/shift-request-by-lm/shift-request-by-lm.component.ts
@@ -1,3 +1,5 @@
+import { ShiftRequestDataService } from './../../services/data/shiftRequest.data';
+import { AppLocalStorageService } from './../../../../services/app-local-storage.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shift-request-by-lm.component.css']
 })
 export class ShiftRequestByLmComponent implements OnInit {
+  screenRole : 'lm';
+  shiftsArray: any;
 
-  constructor() { }
+  constructor(private appLocalStorage: AppLocalStorageService,
+    private shiftRequest : ShiftRequestDataService) { }
 
   ngOnInit(): void {
+    this.screenRole = 'lm';
+    this.getShiftList();
+    
   }
 
+  async getShiftList(){
+    const data = await this.shiftRequest.getDefaultList(this.screenRole);
+    let shifts = data["data"]["payload"];
+    if(!Array.isArray(shifts)){
+      console.log('error occured');
+    }
+    this.shiftsArray = shifts;
+  }
 }
