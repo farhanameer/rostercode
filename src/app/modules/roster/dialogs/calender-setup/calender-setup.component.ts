0707dc662285047ed14a16926b83b967fd02480d
ibtesam.tahir.appsgenii <ibtesam.tahir@appsgenii.eu>
2022-10-17 14:57:48 +0500

    pull 17th

--- src/app/modules/roster/dialogs/calender-setup/calender-setup.component.ts
+++ src/app/modules/roster/dialogs/calender-setup/calender-setup.component.ts
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { AppLocalStorageService } from './../../../../services/app-local-storage.service';
 import { HolidayDataService } from './../../services/data/holidays.data.service';
 import { Component, OnInit } from '@angular/core';
@@ -12,6 +13,10 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 export class CalenderSetupComponent implements OnInit {
   workCalenderSetupForm:FormGroup
   weekDays = [];
+  years = [];
+  current;
+  holidayStatus;
+
   constructor(private fb:FormBuilder, public activeModal: NgbActiveModal,
     private holiday: HolidayDataService,
     private appLocalStorage : AppLocalStorageService) { }
@@ -24,13 +29,29 @@ export class CalenderSetupComponent implements OnInit {
       halfDay:["",Validators.required],
       from:["",Validators.required],
       to:["",Validators.required]
-
-
     });
-
+    this.getYears();
     this.weekends();
     this.getWorkCalendarSetting();
     this.workCalendarSetting();
+  }
+
+  getYears(){
+    this.current = moment();
+    let obj = {
+      id : 0,
+      name :  moment(this.current).format('YYYY')
+    }
+    this.years.push(obj);
+
+    for(let i = 1; i < 10; i++){
+      this.current = moment(this.current).add(1, 'year').format('YYYY');
+      this.years.push({
+        id : i,
+        name : this.current
+      })
+    }
+    console.log(this.years);
   }
 
   async workCalendarSetting(){
@@ -110,12 +131,15 @@ export class CalenderSetupComponent implements OnInit {
   }
 
   async getWorkCalendarSetting(){
+    debugger;
     const params = {
       "client_id" : this.appLocalStorage.getClientId(),
       "year" : 2022,
       "country_id" : 154
     }
     const res = await this.holiday.getWorkCalendarSetting(params); 
+    this.holidayStatus = res['data'].publicHolidays;
+    debugger;
   }
   
   async weekends(){
