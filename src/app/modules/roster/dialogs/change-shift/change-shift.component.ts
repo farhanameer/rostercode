b357958bc9cb40b057afb352077ffb6e9e485215
Farhan Ameer <farhan.ameer@appsgenii.eu>
2022-12-19 13:15:47 +0500

    fixed

--- src/app/modules/roster/dialogs/change-shift/change-shift.component.ts
+++ src/app/modules/roster/dialogs/change-shift/change-shift.component.ts
@@ -63,7 +63,7 @@ export class ChangeShiftComponent implements OnInit, OnChanges {
       }
 
 
-      debugger;
+      
           
       this.submitBtn = false;
       if(body.replaceWithEmployeeId){
@@ -128,6 +128,9 @@ export class ChangeShiftComponent implements OnInit, OnChanges {
   async swapShift(body){
     body["linemanager_id"] = await this.appLocalStorage.getLineManagerId();
     const res = await this.rosterService.swapShift(body);
+    if(res["status"]) {
+      this.activeModal.close(true);
+    }
     
   }
   screenRole = "lm";
